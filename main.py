9db7948378fa0def129f5f1f73b81544e5ea39a0
tahmid1406 <tahmidpranto1406@gmail.com>
2021-12-21 19:25:59 +0600

    prediction using ML model

--- main.py
+++ main.py
@@ -4,16 +4,41 @@ from blockchain import *
 from flask_sqlalchemy import SQLAlchemy 
 from datetime import datetime
 import pickle
-import numpy as np
 import os
+import numpy as np
+import pandas as pd
+import os
+import math
+from numpy import * 
+import random
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, accuracy_score, roc_auc_score # import accuracy metrics
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.linear_model import SGDClassifier
+from sklearn.metrics import recall_score
+from sklearn.metrics import confusion_matrix
+import pickle
+import warnings
+warnings.filterwarnings("ignore")
+
+
+
 
 app = Flask(__name__)
 
 app.secret_key = os.urandom(24)
 
-LOGGED = False
-MODEL = pickle.load(open('DSLModel.pkl', 'rb'))
+
+MODEL = pickle.load(open('Models/DSLModel_1.pkl', 'rb'))
+
+MODEL_tweak = MODEL
+
 USER_DIR = 'users/'
+MODEL_DIR = 'static/models/'
 
 def updatehash(*args):
     hashing_text = ""; h = sha256()
@@ -60,16 +85,30 @@ def make_query():
 
 
 
-
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('index.html', user= g.user)
+    return render_template('index.html', user= g.user, role=g.role)
 
 
+@app.route('/initialTraining', methods=['POST', 'GET'])
+def initialTraining():
+    fileno = len(os.listdir(MODEL_DIR))
+    if request.method == 'POST':
+        trainFile = request.files['trainFile']
+        testFile = request.files['testFile'] 
+        
+        train_filepath = os.path.join(MODEL_DIR, "train.csv")
+        test_filepath = os.path.join(MODEL_DIR, "test.csv")
+        
+        trainFile.save(train_filepath)
+        testFile.save(test_filepath)
+    return render_template('initialTraining.html', user= g.user, role=g.role)
+    
 
 @app.route('/dashboard', methods=['POST', 'GET'])
 def dashboard():
     return render_template('dashboard.html')
+
 
 @app.route('/login' , methods=['POST', 'GET'])
 def login():
@@ -78,7 +117,6 @@ def login():
         session.pop('user', None)
         uname = request.form.get('name')
         passw = request.form.get('password')
-        
         users = sorted(os.listdir(USER_DIR), key=lambda x : int(x))
         
         for user in users[0:]:
@@ -86,6 +124,7 @@ def login():
                 user = json.load(f)
                 if (user.get('name') == uname and user.get('pass1')== passw):
                     session['user'] = user.get('hash')
+                    session['role'] = user.get('role')
                     return redirect(url_for('index'))
                 else:
                     login_text = "username " + uname + " not found in the datebase"
@@ -95,17 +134,26 @@ def login():
 
 @app.route('/register' , methods=['POST', 'GET'])
 def register():
+    user_role = None
     if request.method == 'POST':
         name = request.form.get('name')
         pass1 = request.form.get('password')
         pass2 = request.form.get('password_2')
+        role = request.form.get('role')
         hash = updatehash(name, pass1)
-
+        if role == '1':
+            user_role = 'Regulator'
+        elif role == '2':
+            user_role = 'Contributor'
+        elif role == '3':
+            user_role = 'User'
+       
         user = {
             'name' : name,
             'pass1' : pass1,
             'pass2' : pass2,
-            'hash' : hash
+            'hash' : hash,
+            'role' : user_role
         }
         user_no = len(os.listdir(USER_DIR))
 
@@ -119,7 +167,11 @@ def register():
 
 @app.route('/train' , methods=['POST', 'GET'])
 def train():
-    
+    if request.method == 'POST':
+        file = request.files['csvfile']
+        print(type(file))
+        
+
     return render_template('train.html')
 
 
@@ -155,9 +207,11 @@ def print_blockchain():
 @app.before_request
 def before_request():
     g.user = None
+    g.role = None
 
     if 'user' in session:
         g.user = session['user']
+        g.role = session['role']
 
 if __name__ == '__main__':
     app.run(debug=True)
